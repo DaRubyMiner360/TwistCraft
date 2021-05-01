4fbcb40b825d42188245f16350d436a3f3a36199
DaRubyMiner360 <darubyminer360@gmail.com>
2021-04-30 20:24:11 -0500

    Fixed!

--- twistcraft/src/main/java/ml/darubyminer360/twistcraft/TwistCraft.java
+++ twistcraft/src/main/java/ml/darubyminer360/twistcraft/TwistCraft.java
@@ -2,6 +2,7 @@ package ml.darubyminer360.twistcraft;
 
 import ml.darubyminer360.twistcraft.commands.TwistCommand;
 
+import ml.darubyminer360.twistcraft.items.ItemManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
